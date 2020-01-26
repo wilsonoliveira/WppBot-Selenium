4d175793dddf804b26ec54396bf6481b3e7de894
Lucas Ribeiro Ferreira <ribeiroferreiralucas@gmail.com>
2020-01-26 16:31:37 -0200

    Clean Project and configure requirements.txt

--- sample_BASE_25070.py
+++
@@ -1,8 +0,0 @@
-from wppbot import WppApi
-
-wpp = WppApi(60)
-# while True:
-# wpp.send_message('WACAO', ':)')
-# messages = wpp.get_messages_chat("Delegação Universicopa 💚", 20)
-# print(len(messages))
-wpp.join_group('https://chat.whatsapp.com/EbsTIzO3X6W7Svmqcx529w')
